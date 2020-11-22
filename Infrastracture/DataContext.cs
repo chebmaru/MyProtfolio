99a9b69e2bfdb3e708f364c18e2d9cab5d1115f5
mchebeb <chebmars@gmail.com>
2020-11-22 20:10:37 +0100

    Add Dbset and ModelBuilder

--- Infrastracture/DataContext.cs
+++ Infrastracture/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +10,27 @@ namespace Infrastracture
     {
         public DataContext(DbContextOptions<DataContext> options):base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValue("NEWID()");
+            modelBuilder.Entity<PortfolioItem>().Property(x => x.Id).HasDefaultValue("NEWID()");
+
+            modelBuilder.Entity<Owner>().HasData(
+                new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    Avatar = "avatar.jpg",
+                    FullName = "Marwene",
+                    Profil  = "Junio .Net Developer"
+                }
+                );
 
         }
+
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
     }
 }
